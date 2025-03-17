53e818e1835ccdeef5a5fe5e408cf24b31a598a4
Uttej Venkata Sastry <uttejvsk@hotmail.com>
2025-03-16 20:25:15 -0500

    .

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,6 @@ import { Button } from "./ui/button";
  */
 const Navbar = () => {
   const pathname = usePathname();
-
   return (
     <nav className="bg-white shadow-md py-4 fixed top-0 left-0 right-0 z-10">
       <div className="container mx-auto px-4 flex justify-between items-center">
@@ -17,22 +16,20 @@ const Navbar = () => {
           Talent Visa
         </Link>
 
-        <Button
-          variant="outline"
-          asChild
-          className="bg-blue-500 text-white hover:text-white"
-        >
-          <Link
-            href={pathname === "/post-job" ? "/" : "/post-job"}
-            className={`${
-              pathname === "/post-job"
-                ? "text-blue-600 font-semibold"
-                : "text-gray-600 hover:text-blue-500"
-            }`}
+        {pathname === "/" && (
+          <Button
+            variant="outline"
+            asChild
+            className="bg-blue-500 text-white hover:text-white"
           >
-            {pathname === "/post-job" ? "Go to Home" : "Post a Job"}
-          </Link>
-        </Button>
+            <Link
+              href="/post-job"
+              className="text-gray-600 hover:text-blue-500"
+            >
+              Post a Job
+            </Link>
+          </Button>
+        )}
       </div>
     </nav>
   );
